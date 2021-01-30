71e388484ef97d9c347d57f4f7062947e2d45e08
franadam <francoisadam@hotmail.be>
2021-01-30 14:05:51 +0100

    Fix server production mode

--- server/app.js
+++ server/app.js
@@ -24,7 +24,7 @@ app.use('/api/products/shop', shopRouter);
 app.use('/api/sites', sitesRouter);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('../client/build'));
+  app.use(express.static('client/build'));
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
   });
