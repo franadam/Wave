7a80e9c09ea62df535c147980424b22e7ba0a199
franadam <francoisadam@hotmail.be>
2021-01-30 11:34:37 +0100

    add heroku

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const path = require('path');
 
 const usersRouter = require('./routes/users');
 const brandsRouter = require('./routes/brands');
@@ -21,5 +22,12 @@ app.use('/api/products/woods', woodsRouter);
 app.use('/api/products/guitars', guitarsRouter);
 app.use('/api/products/shop', shopRouter);
 app.use('/api/sites', sitesRouter);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('../client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
+  });
+}
 
 module.exports = app;
